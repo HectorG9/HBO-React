44039f08ce445a1671b06128038bf6aaaa8e8166
Marlon <marzonhapacheco@gmail.com>
2024-07-08 17:34:39 -0300

    Funcionou O erro era por conta de letra minuscula

--- hbo-react/src/components/Principal.jsx
+++ hbo-react/src/components/Principal.jsx
@@ -9,8 +9,7 @@ export default function principal() {
             <h1 className={style.logintitle}>Entrar</h1>
             <label className={style.loginlabel}> Você tem uma conta da HBO Max? </label>
             <input className={style.loginfield} type="email" name="email" id="email" placeholder="Endereço de e-mail" />
-            <input className={style.loginfield} type="password" name="password" id="password" placeholder="Senha" minlength="8" />
-
+            <input className={style.loginfield} type="password" name="password" id="password" placeholder="Senha" minLength="8" maxLength="10"/>
             <div className={style.loginactions}>
               <button type="submit" className={style.button.buttongradient}>Entrar</button>
               <a href="#" className={style.loginlink}> Esqueceu a senha? </a>
