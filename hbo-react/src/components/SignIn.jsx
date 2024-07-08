e0c46844476d65ceba57102cf7b8cd0aadf90a0d
Marlon <marzonhapacheco@gmail.com>
2024-07-08 17:34:50 -0300

    Add o Footer

--- hbo-react/src/components/SignIn.jsx
+++ hbo-react/src/components/SignIn.jsx
@@ -1,13 +1,17 @@
 import style from '../css/signin.module.css';
 
-
+import Principal from './Principal.jsx';
+import Footer from './Footer.jsx';
 
 export default function SignIn() {
   return (
     <>
       <div className={style.bgf}>
 
-       
+        <Principal/>
+
+        <Footer/>
+
       </div>
     </>
   )
