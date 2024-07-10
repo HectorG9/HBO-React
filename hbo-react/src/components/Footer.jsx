ca9417011df9ae48e419c28d4d93b549dced600f
Marlon <marzonhapacheco@gmail.com>
2024-07-09 21:27:55 -0300

    troquei a tag a por link Mesmo assim está dando erro, dizendo que a algo n é decendente da tag a, provavelmente o mesmo acontece no footer

--- hbo-react/src/components/Footer.jsx
+++ hbo-react/src/components/Footer.jsx
@@ -1,8 +1,32 @@
+import { Link } from "react-router-dom";
+
+import style from './signin.module.css'
+
 
 export default function footer() {
   return (
-    <footer>
-        
-    </footer>
+    <>
+      <footer className={style.footer}>
+        <div className={style.container}>
+          <Link to="/images">
+          <Link to="/images">
+            <img src="./asstes/images/max_originals.png" alt="HBO Max" />
+          </Link>
+          <Link to="/images">
+            <img src="assets/images/dc.png" alt="DC" />
+          </Link>
+          <Link to="/images">
+            <img src="assets/images/cartoon-network_neutral.png" alt="CN" />
+          </Link>
+          <Link to="/images">
+            <img src="assets/images/logo_0.png" alt="HBO Max" />
+          </Link>
+          </Link>
+        </div>
+      </footer>
+    </>
   )
 }
+      {/* <nav>
+        <Link to=""></Link>
+      </nav>*/}
--- hbo-react/src/components/Principal.jsx
+++ hbo-react/src/components/Principal.jsx
@@ -1,4 +1,5 @@
 import style from './signin.module.css';
+import { Link } from 'react-router-dom';
 
 export default function principal() {
   return (
@@ -9,10 +10,12 @@ export default function principal() {
             <h1 className={style.logintitle}>Entrar</h1>
             <label className={style.loginlabel}> Você tem uma conta da HBO Max? </label>
             <input className={style.loginfield} type="email" name="email" id="email" autoComplete="Username" placeholder="Endereço de e-mail" />
-            <input className={style.loginfield} type="password" name="password" id="password" autoComplete="current-password" placeholder="Senha" minLength="8" maxLength="10"/>
+            <input className={style.loginfield} type="password" name="password" id="password" autoComplete="current-password" placeholder="Senha" minLength="8" maxLength="10" />
             <div className={style.loginactions}>
-              <button className="button"type="submit">Entrar</button>
-              <a className={style.loginlink} style={{}} href="#" > Esqueceu a senha? </a>
+              <button className={style.button_buttongradient} type="submit">Entrar</button>
+              
+                <Link className={style.loginlink} to="/">Esqueceu a senha?</Link>
+
             </div>
           </form>
         </div>
